82df37110662dcc6d302f09c71aa1e9fd86d22ef
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-07-19 14:28:54 -0500

    front end skeleton done, need to fix models and server

---
+++ client/src/Components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav>
+            <h2>Oh Behave</h2>
+            <a href="/">Login</a>
+        </nav>
+    )
+};
+
+export default Nav;
---
+++ client/src/Components/Splash/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Splash = (props) => {
+    return (
+        <div>
+            <h4>Welcome to Behavior Tracker!</h4>
+            <p>Please log in or create an account to continue.</p>
+        </div>
+    )
+};
+
+export default Splash;
