d11db702675430f864e2429019850596556f73d5
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-08-18 09:22:13 -0500

    moved express static code

--- server.js
+++ server.js
@@ -21,16 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.set('useFindAndModify', false);
 
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.configure(express.rest());
 app.configure(socketio());
@@ -105,6 +96,17 @@ app.get("/api/data/:id", function(req, res) {
 });
 
 app.use(express.errorHandler());
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
