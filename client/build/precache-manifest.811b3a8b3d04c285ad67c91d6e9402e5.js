a116e2768408ffdb48fbe93b5f8e7b62594405d0
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-08-17 20:50:57 -0500

    fixed gitignore

---
+++ client/build/precache-manifest.811b3a8b3d04c285ad67c91d6e9402e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418c9dd3a2e762ce3dc25890a4883def",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5f24e8b75abcf758b62",
+    "url": "/static/css/main.19a8e3be.chunk.css"
+  },
+  {
+    "revision": "699ed9897d7c734717c6",
+    "url": "/static/js/2.5a59c8fb.chunk.js"
+  },
+  {
+    "revision": "d5f24e8b75abcf758b62",
+    "url": "/static/js/main.3190ffd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
